6dc425d440ee1f41a19fad71131c44160929530a
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-02-04 15:47:47 +0100

    feat: import pictures from new file

--- src/productData/JalabiyaDatas.js
+++ src/productData/JalabiyaDatas.js
@@ -1,99 +1,313 @@
-import image1 from "/src/assets/images/MEN/Jalabiya/Jalabiya-3.webp";
-import image2 from "/src/assets/images/MEN/Jalabiya/Jalabiya-4.webp";
-import image3 from "/src/assets/images/MEN/Jalabiya/DSC05551-Edit.webp";
-import image4 from "/src/assets/images/MEN/Jalabiya/Jalabiya-2.webp";
-import image5 from "/src/assets/images/MEN/Jalabiya/Jalabiya-5.webp";
-import image6 from "/src/assets/images/MEN/Jalabiya/Jalabiya-6.webp";
-import image7 from "/src/assets/images/MEN/Jalabiya/Jalabiya-7.webp";
-import image8 from "/src/assets/images/MEN/Jalabiya/Jalabiya-8.webp";
-import image9 from "/src/assets/images/MEN/Jalabiya/Jalabiya-9.webp";
-import image10 from "/src/assets/images/MEN/Jalabiya/Jalabiya-9.webp";
-import image11 from "/src/assets/images/MEN/Jalabiya/Jalabiya-10.webp";
-import image12 from "/src/assets/images/MEN/Jalabiya/Jalabiya-11.webp";
-import image13 from "/src/assets/images/MEN/Jalabiya/Jalabiya-12.webp";
-import image14 from "/src/assets/images/MEN/Jalabiya/Jalabiya-12.webp";
-import image15 from "/src/assets/images/MEN/Jalabiya/Jalabiya-13.webp";
-import image16 from "/src/assets/images/MEN/Jalabiya/Jalabiya-14.webp";
+import image1 from "/src/assets/images/Perfume/perfume1.jpeg";
+import image2 from "/src/assets/images/Perfume/perfume2.jpeg";
+import image3 from "/src/assets/images/Perfume/perfume3.jpeg";
+import image4 from "/src/assets/images/Perfume/perfume4.jpeg";
+import image5 from "/src/assets/images/Perfume/perfume5.jpeg";
+import image6 from "/src/assets/images/Perfume/perfume6.jpeg";
+import image7 from "/src/assets/images/Perfume/perfume7.jpeg";
+import image8 from "/src/assets/images/Perfume/perfume8.jpeg";
+import image9 from "/src/assets/images/Perfume/perfume9.jpeg";
+import image10 from "/src/assets/images/Perfume/perfume10.jpeg";
+import image11 from "/src/assets/images/Perfume/perfume11.jpeg";
+import image12 from "/src/assets/images/Perfume/perfume12.jpeg";
+import image13 from "/src/assets/images/Perfume/perfume13.jpeg";
+import image14 from "/src/assets/images/Perfume/perfume14.jpeg";
+import image15 from "/src/assets/images/Perfume/perfume15.jpeg";
+import image16 from "/src/assets/images/Perfume/perfume16.jpeg";
+import image17 from "/src/assets/images/Perfume/perfume17.jpeg";
+import image18 from "/src/assets/images/Perfume/perfume18.jpeg";
+import image19 from "/src/assets/images/Perfume/perfume19.jpeg";
+import image20 from "/src/assets/images/Perfume/perfume20.jpeg";
+import image21 from "/src/assets/images/Perfume/perfume21.jpeg";
+import image22 from "/src/assets/images/Perfume/perfume22.jpeg";
+import image23 from "/src/assets/images/Perfume/perfume23.jpeg";
+import image24 from "/src/assets/images/Perfume/perfume24.jpeg";
+import image25 from "/src/assets/images/Perfume/perfume25.jpeg";
+import image26 from "/src/assets/images/Perfume/perfume26.jpeg";
+import image27 from "/src/assets/images/Perfume/perfume27.jpeg";
+import image28 from "/src/assets/images/Perfume/perfume28.jpeg";
+import image29 from "/src/assets/images/Perfume/perfume29.jpeg";
+import image30 from "/src/assets/images/Perfume/perfume30.jpeg";
+import image31 from "/src/assets/images/Perfume/perfume31.jpeg";
 
 export const JalabiyaDatas = [
   {
-    id: "noble-jalabiya",
-    name: " Man Hand-painted Jalabiya",
-    price: 255000,
-    oldPrice: 300000,
-    inStock: 5,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "RIGGS",
+    name: "RIGGS SPRAY",
+    price: 4000,
+    inStock: 27,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image1,
+    hoverImage: image1,
+  },
+  {
+    id: "METALOT",
+    name: "METALOT PERFUME",
+    price: 18000,
+    inStock: 18,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image2,
     hoverImage: image2,
   },
   {
-    id: "line-art-jalabiya",
-    name: "Line Art Jalabiya and Cap Set",
-    price: 255000,
-    oldPrice: 300000,
-    inStock: 7,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "ASAD",
+    name: "ASAD PERFUME",
+    price: 25000,
+    inStock: 6,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image3,
+    hoverImage: image3,
+  },
+  {
+    id: "RIGGS",
+    name: "RIGGS PERFUME",
+    price: 4000,
+    inStock: 19,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image4,
     hoverImage: image4,
   },
   {
-    id: "Hand-painted-jalabiya",
-    name: "Hand painted V-Neck  Jalabiya",
-    price: 318000,
-    oldPrice: 420000,
-    inStock: 6,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "RIGGS",
+    name: "RIGGS PERFUME",
+    price: 4000,
+    inStock: 13,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image5,
+    hoverImage: image5,
+  },
+  {
+    id: "TAG",
+    name: "TAG ULTRA DAY POWERFUL DEFENSE DEODORANT",
+    price: 1500,
+    inStock: 6,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image6,
     hoverImage: image6,
   },
   {
-    id: "Line-art-jalabiya",
-    name: "Line Art Jalabiya",
-    price: 255000,
-    oldPrice: 300000,
-    inStock: 10,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "INTENSE-MAN",
+    name: "INTENSE-MAN PERFUME",
+    price: 18000,
+    inStock: 15,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image7,
+    hoverImage: image7,
+  },
+  {
+    id: "SUGAR",
+    name: "SUGAR PERFUME",
+    price: 9000,
+    inStock: 8,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image8,
     hoverImage: image8,
   },
   {
-    id: "Map-of-africa-jalabiya",
-    name: "Map of Africa Hand Painted Jalabiya",
-    price: 255000,
-    oldPrice: 350000,
-    inStock: 13,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "HYPNOTIC-POISON",
+    name: "HYPNOTIC-POISON DIOR",
+    price: 300000,
+    inStock: 16,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image9,
+    hoverImage: image9,
+  },
+  {
+    id: "HAYAATI",
+    name: "HAYAATI PERFUME",
+    price: 28000,
+    inStock: 7,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image10,
     hoverImage: image10,
   },
   {
-    id: "Male-hand-painted-jalabiya",
-    name: "Male Hand-Painted Jalabiya",
-    price: 255000,
-    oldPrice: 300000,
-    inStock: 6,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "CHOKO-MUSK",
+    name: "CHOKO-MUSK PERFUME",
+    price: 6000,
+    inStock: 19,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image11,
+    hoverImage: image11,
+  },
+  {
+    id: "SAUVAGE",
+    name: "SAUVAGE DIOR PERFUME",
+    price: 90000,
+    inStock: 8,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image12,
     hoverImage: image12,
   },
   {
-    id: "Benin-jalabiya",
-    name: "Benin Head Painted Jalabiya",
-    price: 255000,
-    oldPrice: 300000,
-    inStock: 15,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "HUGO",
+    name: "HUGO BOSS PERFUME",
+    price: 3000,
+    inStock: 8,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image13,
+    hoverImage: image13,
+  },
+  {
+    id: "HUG",
+    name: "HUG PERFUME",
+    price: 5000,
+    inStock: 28,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image14,
     hoverImage: image14,
   },
   {
-    id: "Line-art-white-jalabiya",
-    name: "Line Art White Jalabiya and Cap Set",
-    price: 255000, 
-    oldPrice: 300000,
-    inStock: 8,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    id: "KEY-ALI",
+    name: "KEY-ALI PERFUME",
+    price: 12000,
+    inStock: 39,
+    sizes: ["25ml", "50ml", "100ml"],
     image: image15,
+    hoverImage: image15,
+  },
+  {
+    id: "SMART",
+    name: "SMART PERFUME",
+    price: 8500,
+    inStock: 14,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image16,
     hoverImage: image16,
+  },
+  {
+    id: "SUAVAGE",
+    name: "SUAVAGE PERFUME",
+    price: 18000,
+    inStock: 8,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image17,
+    hoverImage: image17,
+  },
+  {
+    id: "LA-AFRICAN",
+    name: "LA-AFRICAN DRUMMER PERFUME",
+    price: 55000,
+    inStock: 9,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image18,
+    hoverImage: image18,
+  },
+  {
+    id: "LIQUID-BRUN",
+    name: "LIQUID-BRUN PERFUME",
+    price: 55000,
+    inStock: 5,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image19,
+    hoverImage: image19,
+  },
+  {
+    id: "NAHEBAT",
+    name: "NAHEBAT-ALJAMAL PERFUME",
+    price: 6500,
+    inStock: 4,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image20,
+    hoverImage: image20,
+  },
+  {
+    id: "KHAMRAH",
+    name: "KHAMRAH PERFUME",
+    price: 8500,
+    inStock: 25,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image21,
+    hoverImage: image21,
+  },
+  {
+    id: "ASAD",
+    name: "ASAD BOURBON PERFUME",
+    price: 25000,
+    inStock: 17,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image22,
+    hoverImage: image22,
+  },
+  {
+    id: "SMART",
+    name: "SMART PERFUME",
+    price: 8500,
+    inStock: 14,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image23,
+    hoverImage: image23,
+  },
+  {
+    id: "SMART",
+    name: "SMART PERFUME",
+    price: 8500,
+    inStock: 8,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image24,
+    hoverImage: image24,
+  },
+  {
+    id: "YARA",
+    name: "YARA PERFUME",
+    price: 8000,
+    inStock: 27,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image25,
+    hoverImage: image25,
+  },
+  {
+    id: "OPUTENT-DUBAI",
+    name: "OPUTENT DUBAI PERFUME",
+    price: 30000,
+    inStock: 20,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image26,
+    hoverImage: image26,
+  },
+  {
+    id: "HAYAATI",
+    name: "HAYAATI PERFUME",
+    price: 28000,
+    inStock: 25,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image27,
+    hoverImage: image27,
+  },
+  {
+    id: "ARMAR-BLING",
+    name: "ARMAR BLING PERFUME",
+    price: 70000,
+    inStock: 5,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image28,
+    hoverImage: image28,
+  },
+  {
+    id: "SUITS",
+    name: "SUITS FRAGRANCE WORLD PERFUME",
+    price: 25000,
+    inStock: 16,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image29,
+    hoverImage: image29,
+  },
+  {
+    id: "STORM-ELIXIR",
+    name: "STORM ELIXIR PERFUME",
+    price: 4500,
+    inStock: 19,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image30,
+    hoverImage: image30,
+  },
+  {
+    id: "OPHILIA",
+    name: "OPHILIA PERFUME",
+    price: 6000,
+    inStock: 14,
+    sizes: ["25ml", "50ml", "100ml"],
+    image: image31,
+    hoverImage: image31,
   },
 ];
