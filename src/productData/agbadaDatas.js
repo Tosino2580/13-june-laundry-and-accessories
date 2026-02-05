11b708ee45968e26f9d65e7d57a81027e215be26
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-02-05 17:51:54 +0100

    feat: import pictures into AgbadaDatas.js

--- src/productData/agbadaDatas.js
+++ src/productData/agbadaDatas.js
@@ -1,63 +1,201 @@
-import image1 from "/src/assets/images/MEN/Agbada/agbada-1.webp";
-import image2 from "/src/assets/images/MEN/Agbada/agbada-2.webp";
-import image3 from "/src/assets/images/MEN/Agbada/agbada-3.webp";
-import image4 from "/src/assets/images/MEN/Agbada/agbada-4.webp";
-import image5 from "/src/assets/images/MEN/Agbada/agbada-5.webp";
-import image6 from "/src/assets/images/MEN/Agbada/agbada-6.webp";
-import image7 from "/src/assets/images/MEN/Agbada/agbada-7.webp";
-import image8 from "/src/assets/images/MEN/Agbada/agbada-8.webp";
-import image9 from "/src/assets/images/MEN/Agbada/agbada-9.webp";
-import image10 from "/src/assets/images/MEN/Agbada/agbada-10.webp";
+import image1 from "/src/assets/images/Bags/bags1.jpeg";
+import image2 from "/src/assets/images/Bags/bags2.jpeg";
+import image3 from "/src/assets/images/Bags/bags3.jpeg";
+import image4 from "/src/assets/images/Bags/bags4.jpeg";
+import image5 from "/src/assets/images/Bags/bags5.jpeg";
+import image6 from "/src/assets/images/Bags/bags6.jpeg";
+import image7 from "/src/assets/images/Bags/bags7.jpeg";
+import image8 from "/src/assets/images/Bags/bags8.jpeg";
+import image9 from "/src/assets/images/Bags/bags9.jpeg";
+import image10 from "/src/assets/images/Bags/bags10.jpeg";
+import image11 from "/src/assets/images/Bags/bags11.jpeg";
+import image12 from "/src/assets/images/Bags/bags12.jpeg";
+import image13 from "/src/assets/images/Bags/bags13.jpeg";
+import image14 from "/src/assets/images/Bags/bags14.jpeg";
+import image15 from "/src/assets/images/Bags/bags15.jpeg";
+import image16 from "/src/assets/images/Bags/bags16.jpeg";
+import image17 from "/src/assets/images/Bags/bags17.jpeg";
+import image18 from "/src/assets/images/Bags/bags18.jpeg";
+import image19 from "/src/assets/images/Bags/bags19.jpeg";
+import image20 from "/src/assets/images/Bags/bags20.jpeg";
+import image21 from "/src/assets/images/Bags/bags21.jpeg";
+import image22 from "/src/assets/images/Bags/bags22.jpeg";
 
 export const agbadaDatas = [
   {
-    id: "Historic-ozo-agbada",
-    name: "The Historic Ozo x NFL Agbada",
-    price: 1273800,
-    oldPrice: 1543000,
+    // id: "Historic-ozo-agbada",
+    // name: "The Historic Ozo x NFL Agbada",
+    price: 25000,
     inStock: 4,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
     image: image1,
+    hoverImage: image1,
+  },
+  {
+    // id: "Macallan-agbada",
+    // name: "Macallan Reserve Agbada",
+    price: 25000,
+    inStock: 7,
+    image: image2,
     hoverImage: image2,
   },
   {
-    id: "Macallan-agbada",
-    name: "Macallan Reserve Agbada",
-    price: 885000,
-    oldPrice: 1000000,
-    inStock: 7,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    // id: "Onìńówọ-Olori-aso-oke-agbada",
+    // name: "Onìńówọ & Olori Patched Aso-Oke Agbada",
+    price: 45000,
+    inStock: 3,
     image: image3,
+    hoverImage: image3,
+  },
+  {
+    // id: "Turquoise-Blue-agbada",
+    // name: "Turquoise Blue Agbada Kaftan and Aso Oke Pant",
+    price: 45000,
+    inStock: 10,
+    image: image4,
     hoverImage: image4,
   },
   {
-    id: "Onìńówọ-Olori-aso-oke-agbada",
-    name: "Onìńówọ & Olori Patched Aso-Oke Agbada",
-    price: 3315000,
-    oldPrice: 3450000,
-    inStock: 3,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 50000,
+    inStock: 7,
     image: image5,
+    hoverImage: image5,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 4,
+    image: image6,
     hoverImage: image6,
   },
   {
-    id: "Turquoise-Blue-agbada",
-    name: "Turquoise Blue Agbada Kaftan and Aso Oke Pant",
-    price: 344300,
-    oldPrice: 400000,
-    inStock: 10,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 4,
     image: image7,
+    hoverImage: image7,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 7,
+    image: image8,
     hoverImage: image8,
   },
   {
-    id: "Lion-king-agbada",
-    name: "Lion King Agbada, Aso Oke Pant and Cap Set",
-    price: 344300,
-    oldPrice: 500000,
-    inStock: 7,
-    sizes: ["S", "M", "L", "XL", "XXL", "3XS"],
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 2,
     image: image9,
+    hoverImage: image9,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 50000,
+    inStock: 14,
+    image: image10,
     hoverImage: image10,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 15000,
+    inStock: 11,
+    image: image11,
+    hoverImage: image11,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 25000,
+    inStock: 16,
+    image: image12,
+    hoverImage: image12,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 50000,
+    inStock: 16,
+    image: image13,
+    hoverImage: image13,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 40000,
+    inStock: 3,
+    image: image14,
+    hoverImage: image14,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 30000,
+    inStock: 19,
+    image: image15,
+    hoverImage: image15,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 30000,
+    inStock: 3,
+    image: image16,
+    hoverImage: image16,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 30000,
+    inStock: 5,
+    image: image17,
+    hoverImage: image17,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 40000,
+    inStock: 7,
+    image: image18,
+    hoverImage: image18,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 40000,
+    inStock: 12,
+    image: image19,
+    hoverImage: image19,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 40000,
+    inStock: 14,
+    image: image20,
+    hoverImage: image20,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 17,
+    image: image21,
+    hoverImage: image21,
+  },
+  {
+    // id: "Lion-king-agbada",
+    // name: "Lion King Agbada, Aso Oke Pant and Cap Set",
+    price: 45000,
+    inStock: 5,
+    image: image22,
+    hoverImage: image22,
   },
 ];
